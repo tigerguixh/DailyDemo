e5ae401e4bef152f9ee0f1ad5ade798058f65efa
xunhu.gui <xunhu.gui@tadu.com>
2018-05-02 10:29:39 +0800

    add a oberver file

---
+++ app/src/main/java/tiger/com/lp/dailydemo/designpatterns/observer/concrete/SubjectObsevable.java
@@ -0,0 +1,12 @@
+package tiger.com.lp.dailydemo.designpatterns.observer.concrete;
+
+import java.util.Observable;
+
+/**
+ * @author : Xunhu.gui
+ * @Email : xunhu.gui@tadu.com
+ * @date : 2018/5/2
+ * @Description :
+ */
+public class SubjectObsevable extends Observable{
+}
