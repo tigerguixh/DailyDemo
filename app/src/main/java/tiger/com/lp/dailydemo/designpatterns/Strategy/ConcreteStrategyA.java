7bbde0ff7687a78b12b170b8b7a26d512677b0d4
xunhu.gui <xunhu.gui@tadu.com>
2018-05-15 10:43:52 +0800

    模式

---
+++ app/src/main/java/tiger/com/lp/dailydemo/designpatterns/Strategy/ConcreteStrategyA.java
@@ -0,0 +1,10 @@
+package tiger.com.lp.dailydemo.designpatterns.Strategy;
+
+/**
+ * @author : Xunhu.gui
+ * @Email : xunhu.gui@tadu.com
+ * @date : 2018/5/12
+ * @Description :
+ */
+public class ConcreteStrategyA {
+}
