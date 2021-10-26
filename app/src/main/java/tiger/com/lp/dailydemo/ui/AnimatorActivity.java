847adf4472320c4fc828172fcd27a1f136aa1906
xunhu.gui <xunhu.gui@tadu.com>
2021-10-26 17:29:48 +0800

    更新

--- app/src/main/java/tiger/com/lp/dailydemo/ui/AnimatorActivity.java
+++ app/src/main/java/tiger/com/lp/dailydemo/ui/AnimatorActivity.java
@@ -3,13 +3,19 @@ package tiger.com.lp.dailydemo.ui;
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Application;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import android.provider.Settings;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+
+import java.lang.reflect.InvocationTargetException;
 
 import tiger.com.lp.dailydemo.R;
 
@@ -51,5 +57,41 @@ public class AnimatorActivity extends Activity {
 
         button.animate().setDuration(3000).rotationYBy(720).x(100).y(100).start();
 
+    }
+
+    public static String getAndroidID() {
+        try {
+            String id = Settings.Secure.getString(
+                    getApplicationByReflect().getContentResolver(),
+                    Settings.Secure.ANDROID_ID
+            );
+            if ("9774d56d682e549c".equals(id)) return "";
+            return TextUtils.isEmpty(id) ? "" : id;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private static Application getApplicationByReflect() {
+        try {
+            @SuppressLint("PrivateApi")
+            Class<?> activityThread = Class.forName("android.app.ActivityThread");
+            Object thread = activityThread.getMethod("currentActivityThread").invoke(null);
+            Object app = activityThread.getMethod("getApplication").invoke(thread);
+            if (app == null) {
+                throw new NullPointerException("u should init first");
+            }
+            return (Application) app;
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        throw new NullPointerException("u should init first");
     }
 }
